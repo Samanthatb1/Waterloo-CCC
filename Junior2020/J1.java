6520a4083e3c99375c244919784a348f20be698d
Samantha Grieco <62915953+Samanthatb1@users.noreply.github.com>
2021-02-23 20:38:46 -0500

    Create J1.java

---
+++ Junior2020/J1.java
@@ -0,0 +1,21 @@
+// Question Source https://cemc.uwaterloo.ca/contests/computing/2020/index.html
+
+import java.io.*;
+
+public class Main11 {
+    public static void main(String[] args) throws IOException {
+    BufferedReader objReader = new BufferedReader(new InputStreamReader (System.in));
+    
+    int small, medium, large;
+
+    small = Integer.parseInt (objReader.readLine ()); 
+    medium = Integer.parseInt (objReader.readLine ()); 
+    large = Integer.parseInt (objReader.readLine ()); 
+
+    if (((small * 1) + (medium * 2) + (large * 3)) >= 10){
+        System.out.println("happy");
+    } else {
+        System.out.println("sad");
+    }
+    }
+}
